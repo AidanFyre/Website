e66245cae335a11868d2444302d8a2a01d75a965
Aidan <support@fyreempire.net>
2023-11-18 16:20:38 -0800

    changed to drag and drop

--- script.js
+++ script.js
@@ -14,3 +14,47 @@ document.addEventListener('DOMContentLoaded', (event) => {
         console.log('Legend Pose:', legendPose);
     });
 });
+document.addEventListener('DOMContentLoaded', (event) => {
+    const dropArea = document.getElementById('dropArea');
+    const imageUpload = document.getElementById('imageUpload');
+
+    // Open file dialog when clicking on the drop area
+    dropArea.addEventListener('click', () => imageUpload.click());
+
+    // Prevent default drag behaviors
+    ['dragenter', 'dragover', 'dragleave', 'drop'].forEach(eventName => {
+        dropArea.addEventListener(eventName, preventDefaults, false);
+    });
+
+    function preventDefaults(e) {
+        e.preventDefault();
+        e.stopPropagation();
+    }
+
+    // Highlight drop area when item is dragged over it
+    ['dragenter', 'dragover'].forEach(eventName => {
+        dropArea.addEventListener(eventName, highlight, false);
+    });
+
+    ['dragleave', 'drop'].forEach(eventName => {
+        dropArea.addEventListener(eventName, unhighlight, false);
+    });
+
+    function highlight(e) {
+        dropArea.classList.add('highlight');
+    }
+
+    function unhighlight(e) {
+        dropArea.classList.remove('highlight');
+    }
+
+    // Handle dropped files
+    dropArea.addEventListener('drop', handleDrop, false);
+
+    function handleDrop(e) {
+        let dt = e.dataTransfer;
+        let files = dt.files;
+
+        // Handle files here (e.g., display preview, add to form data, etc.)
+    }
+});
