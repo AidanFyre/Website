4d956ba13061b94635c9080e33f6c4f8aaee4354
Aidan <support@fyreempire.net>
2023-11-20 15:28:29 -0800

    dshdisah

--- js/lib/supabaseClient.js
+++ js/lib/supabaseClient.js
@@ -1,9 +1,9 @@
 // lib/supabaseClient.js™
 import { createClient } from 'https://esm.sh/@supabase/supabase-js@2'
 
-const supabaseUrl = NEXT_PUBLIC_SUPABASE_URL;
-const supabaseAnonKey = NEXT_PUBLIC_SUPABASE_ANON_KEY;
+// Use the environment variables set in Vercel
+const supabaseUrl = NEXT_PUBLIC_SUPABASE_URL; // Replace with your Supabase URL variable name
+const supabaseAnonKey = NEXT_PUBLIC_SUPABASE_ANON_KEY; // Replace with your Supabase Anon Key variable name
 
+// Create a single Supabase client for interacting with your database
 const supabase = createClient(supabaseUrl, supabaseAnonKey);
-
-export default supabase;
