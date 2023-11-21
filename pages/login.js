1b28de8f959e4318df71b57bbadb9130051c9e20
Aidan <support@fyreempire.net>
2023-11-20 16:02:17 -0800

    test

---
+++ pages/login.js
@@ -0,0 +1,17 @@
+import supabase from '../lib/supabaseClient';
+
+export default function LoginPage() {
+  const handleLogin = async () => {
+    const { user, session, error } = await supabase.auth.signIn({
+      provider: 'discord',
+    });
+
+    // Handle login response
+  };
+
+  return (
+    <div>
+      <button onClick={handleLogin}>Login with Discord</button>
+    </div>
+  );
+}
